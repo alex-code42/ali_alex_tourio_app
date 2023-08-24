42fa7129effdbc5300da8930409ef29d922823b6
alihinnawe <hinnaweali@gmail.com>
2023-08-24 15:03:11 +0200

    pushing new place to the database and viewing it on the browser

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,7 +5,27 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const places = await Place.find();
-    console.log("places are in API: ", places);
+    // console.log("places are in API: ", places);
     return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      // We're declaring jokeData to contain the body of our request sent by our form that we haven't created yet.
+      // The body of our request might contain data in a variety of formats, but is typically an object.
+      const place = new Place(placeData);
+      // Utilizing our Joke scheme, we're creating a new joke.
+      // At this point we're sanitizing our data according to the schema of our Joke model.
+      await place.save();
+      // We've created a new joke, now we're calling save() to have mongoose insert a new document into our database.
+      // The three lines above are functionally the same as:
+      // Joke.create(request.body)
+      // It's just a somewhat less opaque way.
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   }
 }
--- pages/create.js
+++ pages/create.js
@@ -1,18 +1,38 @@
-import Link from 'next/link.js';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import Form from '../components/Form.js';
-import { StyledLink } from '../components/StyledLink.js';
-
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import styled from "styled-components";
+import Form from "../components/Form";
+import { StyledLink } from "../components/StyledLink.js";
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
-
 export default function CreatePlacePage() {
+  // const { mutate } = useSWR("/api/places");
   const router = useRouter();
 
-  function addPlace(place) {
-    console.log('Place added (but not really...)');
+  async function addPlace(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const locationData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/places", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(locationData),
+    });
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    } else if (response.ok) {
+      await response.json();
+      router.push("/");
+    }
+    // mutate();
+    event.target.reset();
   }
 
   return (
@@ -21,7 +41,7 @@ export default function CreatePlacePage() {
       <Link href="/" passHref legacyBehavior>
         <StyledBackLink>back</StyledBackLink>
       </Link>
-      <Form onSubmit={addPlace} formName={'add-place'} />
+      <Form onSubmit={addPlace} formName={"add-place"} />
     </>
   );
 }
